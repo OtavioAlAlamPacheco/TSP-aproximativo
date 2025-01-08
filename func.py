55ee9d54e9723f242dfce16b1a462e879e9345c1
Luis Eduardo <fnthon.skja@gmail.com>
2025-01-08 19:38:29 -0300

    Organização das funções e adição de métrica de tempo de execução

--- func.py
+++ func.py
@@ -1,67 +1,71 @@
 import os
 
-def ler_matriz(file):
+def ler_matriz(arquivo):
     """
     Lê uma matriz a partir de um arquivo.
+    
     Argumentos:
-        file (str): O caminho para o arquivo que contém a matriz.
+        arquivo (str): O caminho para o arquivo que contém a matriz.
+    
     Retorna:
         list: Uma lista de listas de inteiros representando a matriz, ou
-        imprime uma mensagem de erro se o arquivo não existir ou se ocorrer um erro ao ler o arquivo.
+        None se o arquivo não existir ou se ocorrer um erro ao ler o arquivo.
     """
-
-    if not os.path.exists(file):
-        print(f"Erro: O arquivo {file} não existe.")
-        return None
+    if not os.path.exists(arquivo):
+        print(f"Erro: O arquivo {arquivo} não existe.")
+        return None    
     
+    with open(arquivo, 'r') as arq:
+        matriz = []
+        for linha in arq:
+            matriz.append([int(x) for x in linha.split()])
+    return matriz
+   
+def caixeiroViajante(arquivo):
+    """
+    Resolve o problema do Caixeiro Viajante usando programação dinâmica.
+    
+    Argumentos:
+        arquivo (str): O caminho para o arquivo que contém a matriz de custos.
+    
+    Retorna:
+        int: O custo mínimo para completar o percurso do Caixeiro Viajante, ou
+        None se ocorrer um erro ao ler o arquivo.
+    """
     try:
-        with open(file, 'r') as arq:
-            matriz = []
-            for linha in arq:
-                matriz.append([int(x) for x in linha.split()])
-        return matriz
-    
+        matriz = ler_matriz(arquivo)
     except Exception as e:
         print(f"Erro ao ler o arquivo: {e}")
         return None
-   
-def caixeiroViajante(custo):
-    """
-    Resolve o problema do Caixeiro Viajante usando programação dinâmica.
-    Este método calcula o custo mínimo para visitar todas as cidades exatamente uma vez e retornar à cidade de origem.
-    Parâmetros:
-        custo (matriz(lista de listas de int)): Uma matriz de custos onde custo[i][j] representa o custo de viajar da cidade i para a cidade j.
-    Retorna:
-        int: O custo mínimo para completar o percurso do Caixeiro Viajante.
-    """
     
-    n = len(custo)
+    n = len(matriz)
 
     # Usar um dicionário para memoização
     memo = {}
 
-    return custoTotal(1, 0, n, custo, memo)
+    return custoTotal(1, 0, n, matriz, memo)
  
 def custoTotal(mascara, atual, n, custo, memo):
     """
     Calcula o custo total mínimo para visitar todas as cidades e retornar à cidade inicial.
-    Parâmetros:
+    
+    Argumentos:
         mascara (int): Um inteiro que representa as cidades visitadas até o momento usando bits.
         atual (int): O índice da cidade atual.
         n (int): O número total de cidades.
         custo (list): Uma matriz 2D onde custo[i][j] representa o custo de viajar da cidade i para a cidade j.
         memo (dict): Um dicionário usado para memoização, onde memo[(atual, mascara)] armazena o custo mínimo calculado para um estado específico.
+    
     Retorna:
         int: O custo total mínimo para visitar todas as cidades e retornar à cidade inicial.
     """
-    
     if mascara == (1 << n) - 1:
         return custo[atual][0]
 
     if (atual, mascara) in memo:
         return memo[(atual, mascara)]
 
-    ans = float('inf') # Inicializar ans com infinito para garantir que qualquer custo encontrado será menor
+    ans = float('inf')  # Inicializar ans com infinito para garantir que qualquer custo encontrado será menor
     
     for proximo in range(n):
         if mascara & (1 << proximo) == 0:
--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-from func import ler_matriz, caixeiroViajante
+import time
+from func import caixeiroViajante
 
 def main():
     """
     Função principal que executa o algoritmo do Caixeiro Viajante.
-    Esta função lê uma matriz de um arquivo especificado, imprime a matriz
-    e, em seguida, calcula e imprime a solução do problema do Caixeiro Viajante
-    usando a função `caixeiroViajante`.
+    Esta função lê uma matriz de um arquivo especificado, calcula e imprime
+    a solução do problema do Caixeiro Viajante usando a função `caixeiroViajante`.
     O arquivo de entrada deve estar localizado no diretório "matrizes" e deve
-    ser especificado pelo caminho relativo "matrizes/".
-    A função `ler_matriz` é utilizada para ler a matriz do arquivo e a função
-    `caixeiroViajante` é utilizada para calcular a solução do problema.
-    Returns:
-        None
+    ser especificado pelo caminho relativo "matrizes/XXX.txt".
     """
-    arquivo = "matrizes/tsp3_1194.txt" # Caminho relativo para o arquivo de entrada, altere conforme necessário.
-    matriz = ler_matriz(arquivo)
     
-    if matriz is not None:
-        print(matriz)
+    começo = time.time()
+    arquivo = "matrizes/tsp4_7013.txt" # Caminho relativo para o arquivo de entrada, altere conforme necessário.
+    
+    print("Resultado do TSP: ", caixeiroViajante(arquivo))
         
-    print(caixeiroViajante(matriz))
-        
+    fim = time.time()
+    print(f"Tempo de execução: {fim - começo} segundos")
+    
+    
 if __name__ == "__main__":
     main()
